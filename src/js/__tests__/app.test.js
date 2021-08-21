285041b1cadf39d44f5298a078f60eaa148452d8
Anna <k_anna_i@mail.ru>
2021-08-21 23:09:42 +0300

    improve tests

--- src/js/__tests__/app.test.js
+++ src/js/__tests__/app.test.js
@@ -6,39 +6,117 @@ import Undead from '../undead';
 import Zombie from '../zombie';
 import Daemon from '../daemon';
 
-test('creation daemon', () => {
-  const hero1 = new Daemon('Mimi');
-  expect(hero1.type).toBe('Daemon');
-});
-
-test('magician defend', () => {
-  const hero2 = new Magician('Harry');
-  expect(hero2.defend).toBe(40);
-});
-
-test('undead attack', () => {
-  const hero3 = new Undead('Lenin');
-  expect(hero3.defend).toBe(25);
-});
-
-test('zombie level', () => {
-  const hero4 = new Zombie('Voldemort');
-  expect(hero4.level).toBe(1);
-});
-
-test('bowermen health', () => {
-  const hero5 = new Bowerman('Betman');
-  expect(hero5.health).toBe(100);
+test('creation bowerman', () => {
+  const hero1 = new Bowerman('Spiderman');
+  const expected = {
+    name: 'Spiderman',
+    type: 'Bowerman',
+    health: 100,
+    level: 1,
+    attack: 25,
+    defend: 25
+  }
+  expect(hero1).toEqual(expected);
 });
 
 test('creation swordsman', () => {
-  const hero6 = new Swordsman('DArtanian');
+  const hero2 = new Swordsman('Halk');
   const expected = {
-    name: 'DArtanian',
+    name: 'Halk',
     type: 'Swordsman',
     health: 100,
+    level: 1,
     attack: 40,
-    defend, 10
+    defend: 10
+  }
+  expect(hero2).toEqual(expected);
+});
+
+test('creation magician', () => {
+  const hero3 = new Magician('Strange');
+  const expected = {
+    name: 'Strange',
+    type: 'Magician',
+    health: 100,
+    level: 1,
+    attack: 10,
+    defend: 40
+  }
+  expect(hero3).toEqual(expected);
+});
+
+test('creation undead', () => {
+  const hero4 = new Undead('Hydra');
+  const expected = {
+    name: 'Hydra',
+    type: 'Undead',
+    health: 100,
+    level: 1,
+    attack: 25,
+    defend: 25
+  }
+  expect(hero4).toEqual(expected);
+});
+
+test('creation zombie', () => {
+  const hero5 = new Zombie('NoName');
+  const expected = {
+    name: 'NoName',
+    type: 'Zombie',
+    health: 100,
+    level: 1,
+    attack: 40,
+    defend: 10
+  }
+  expect(hero5).toEqual(expected);
+});
+
+test('creation daemon', () => {
+  const hero6 = new Daemon('Mimi');
+  const expected = {
+    name: 'Mimi',
+    type: 'Daemon',
+    health: 100,
+    level: 1,
+    attack: 10,
+    defend: 40
+  }
+  expect(hero6).toEqual(expected);
+});
+
+test('damage', () => {
+  const hero6 = new Daemon('Mimi');
+  hero6.damage(80);
+  expect(hero6.health).toBe(52);
+});
+
+test('level up', () => {
+  const hero6 = new Daemon('Mimi');
+  hero6.damage(80);
+  hero6.levelUp();
+  hero6.levelUp();
+  const expected = {
+    name: 'Mimi',
+    type: 'Daemon',
+    health: 100,
+    level: 3,
+    attack: 14.4,
+    defend: 57.6
+  }
+  expect(hero6).toEqual(expected);
+});
+
+test('kelled', () => {
+  const hero6 = new Daemon('Mimi');
+  hero6.damage(800);
+  hero6.levelUp();
+  const expected = {
+    name: 'Mimi',
+    type: 'Daemon',
+    health: -380,
+    level: 1,
+    attack: 10,
+    defend: 40
   }
   expect(hero6).toEqual(expected);
 });
